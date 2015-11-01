9a24966b414a41ac2d91c214a0b4f7fd5b440866
claudiopro <claudio.procida@gmail.com>
2015-11-01 23:02:10 +0100

    Footer

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -12,13 +12,13 @@ var App = React.createClass({
               <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88"/>
             </a>
     				<a href="http://jigsaw.w3.org/css-validator/check/referer">
-              <img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"/>
+              <img className="vcss" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"/>
             </a>
     			</p>
     			<p id="credits">
-    				<a href="http://www.emeraldion.it">Emeraldion Lodge</a>  — © 2001-2015 Claudio Procida
+    				<a href="http://www.emeraldion.it">Emeraldion Lodge</a> © 2001-2015 Claudio Procida
     			</p>
-    			<p id="disclaimer" style="text-align:center;color:#999">
+    			<p id="disclaimer">
     				Apple, the Apple logo, Macintosh, Mac OS, MacBook, MacBook Pro, MacBook Air, iBook and PowerBook are registered trademark of Apple Inc.
     			</p>
         </footer>
